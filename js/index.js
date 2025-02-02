965c417c7bb6a2c1adc93aa436cc15f21dd5588a
matheus <matheus.com>
2025-02-02 13:59:17 -0300

    Foi criado as classes dos personagens e modifiquei os arquivos JS para um file diferente

---
+++ js/index.js
@@ -0,0 +1,8 @@
+let char = new Sorcerer('Gordof');
+let monster = new BigMonster();
+let monster2 = new LittleMonster();
+
+console.log(monster.name);
+console.log(monster2.name);
+console.log(monster2.attack);
+console.log(monster.attack);
